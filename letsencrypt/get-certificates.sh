78686aadfb3f0d798fd521b7eb55acf71b7bca3b
root <root@docker.aaronlenoir.com>
2017-03-15 01:01:54 +0000

    Adds letsencrypt script and ssl configuration for nginx.conf.

---
+++ letsencrypt/get-certificates.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker run --rm -p 80:80 -p 443:443 -v $PWD/etc/letsencrypt:/etc/letsencrypt -v $PWD/var/log/letsencrypt:/var/log/letsencrypt certbot/certbot certonly --standalone --agree-tos -v -c /etc/letsencrypt/cli.ini -d news.aaronlenoir.com
+cp $PWD/etc/letsencrypt/live/news.aaronlenoir.com/*.pem /etc/ssl/news.aaronlenoir.com/
