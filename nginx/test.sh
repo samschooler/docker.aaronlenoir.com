a97fdfa9026f91d77cc85593ba44f2be4a12c997
AaronLenoir <lenoir.aaron@gmail.com>
2017-03-18 22:02:30 +0000

    Cleans up nginx.conf a bit.

---
+++ nginx/test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+./down.sh
+docker run --rm -v $PWD/nginx:/etc/nginx:ro -v /etc/ssl/news.aaronlenoir.com:/etc/ssl/news.aaronlenoir.com -v /etc/ssl/blog.aaronlenoir.com:/etc/ssl/blog.aaronlenoir.com -p 80:80 -p 443:443 --link news-reader:news-reader --link ghost:ghost --name nginx_proxy nginx 
+
