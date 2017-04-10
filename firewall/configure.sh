09d4eb995438e4fa0a2f2d9fcfb48e9a9691cbeb
AaronLenoir <lenoir.aaron@gmail.com>
2017-04-10 22:26:27 +0000

    Added update of Ghost theme.

--- firewall/configure.sh
+++ firewall/configure.sh
@@ -4,4 +4,4 @@ ufw default allow outgoing
 ufw allow ssh
 ufw allow http
 ufw allow https
-ufw deny from 146.185.133.247 to any
+ufw deny from 146.185.133.247
--- update.sh
+++ update.sh
@@ -4,3 +4,5 @@ docker pull aaronlenoir/news-reader
 docker pull mysql
 docker pull digit/wait-for-mysql
 docker pull ghost
+echo "Updating ghost theme ..."
+git -C blog/ghost/data/ghost/var/lib/ghost/themes/casper-light/ pull
