241b2ef4b54ce76934fa159ae5ae034d560cef4b
AaronLenoir <lenoir.aaron@gmail.com>
2017-10-29 23:01:15 +0000

    Ensured ghost could upgrade to 0.11.12 and later.

--- blog/ghost/up.sh
+++ blog/ghost/up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . ./scripts/settings.sh
-docker run --rm --name $container_name -v $PWD/data/$container_name/var/lib/ghost:/var/lib/ghost -v $PWD/config/config.js:/var/lib/ghost/config.js -e "NODE_ENV=production" --link $mysql_container_name:mysql -d ghost
+docker run --rm --name $container_name -v $PWD/data/$container_name/var/lib/ghost:/var/lib/ghost -v $PWD/config/config.js:/var/lib/ghost/config.js -e "NODE_ENV=production" --link $mysql_container_name:mysql -d ghost:0
--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ docker pull nginx
 docker pull aaronlenoir/news-reader
 docker pull mysql
 docker pull digit/wait-for-mysql
-docker pull ghost
+docker pull ghost:0
 echo "Updating ghost theme ..."
 git -C blog/ghost/data/ghost/var/lib/ghost/themes/casper-light/ pull
 git -C sleepy pull
