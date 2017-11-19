9b5c33e7b6468ff861b5aa66cc9754abcc1d5518
AaronLenoir <lenoir.aaron@gmail.com>
2017-11-19 23:36:22 +0000

    Removes sleepy website because not used anymore.

--- nginx/test.sh
+++ nginx/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 docker run --rm nginx nginx -V
 ./down.sh
-docker run --rm -v /root/sleepy:/sleepy:ro -v $PWD/nginx:/etc/nginx:ro -v /etc/ssl/news.aaronlenoir.com:/etc/ssl/news.aaronlenoir.com -v /etc/ssl/blog.aaronlenoir.com:/etc/ssl/blog.aaronlenoir.com -p 80:80 -p 443:443 --link news-reader:news-reader --link ghost:ghost --name nginx_proxy nginx 
+docker run --rm -v $PWD/nginx:/etc/nginx:ro -v /etc/ssl/news.aaronlenoir.com:/etc/ssl/news.aaronlenoir.com -v /etc/ssl/blog.aaronlenoir.com:/etc/ssl/blog.aaronlenoir.com -p 80:80 -p 443:443 --link news-reader:news-reader --link ghost:ghost --name nginx_proxy nginx 
 
