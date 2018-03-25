b17305ec0e9253caad49ebf243b8d2516a506095
Sam Schooler <me@sam.ink>
2018-03-24 20:42:37 -0400

    Update up.sh

--- nginx/up.sh
+++ nginx/up.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -v $PWD/nginx:/etc/nginx:ro -v /etc/ssl/news.aaronlenoir.com:/etc/ssl/news.aaronlenoir.com -v /etc/ssl/blog.aaronlenoir.com:/etc/ssl/blog.aaronlenoir.com -p 80:80 -p 443:443 --link news-reader:news-reader --link ghost:ghost --name nginx_proxy -d nginx 
+docker run --rm -v $PWD/nginx:/etc/nginx:ro -v /etc/ssl/samschooler.me:/etc/ssl/samschooler.me -v /etc/ssl/sam.ink:/etc/ssl/sam.ink -p 80:80 -p 443:443 --link portfolio:portfolio --link ghost:ghost --name nginx_proxy -d nginx 
