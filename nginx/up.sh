a6cb19d96437d70eb68d9951d5acfd11a6adebcb
Sam Schooler <me@sam.ink>
2018-03-25 14:39:25 -0600

    add ghost

--- nginx/up.sh
+++ nginx/up.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -v $PWD/nginx/nginx:/etc/nginx:ro -v /etc/ssl/samschooler.me:/etc/ssl/samschooler.me -v /etc/ssl/sam.ink:/etc/ssl/sam.ink -v $PWD/data/var/www:/var/www -p 80:80 -p 443:443 --link ghost:ghost --name nginx_proxy -d nginx
+docker run --rm -v $PWD/nginx/nginx:/etc/nginx:ro -v /etc/ssl/samschooler.me:/etc/ssl/samschooler.me -v /etc/ssl/sam.ink:/etc/ssl/sam.ink -v $PWD/data/var/www:/var/www -v $PWD/nginx/nginx/mime.types:/etc/nginx/mime.types -p 80:80 -p 443:443 --link ghost:ghost --name nginx_proxy -d nginx
