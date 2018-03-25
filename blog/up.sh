f3daf4aab18eb35b0ba8b75367bf7c46a705799d
Sam Schooler <me@sam.ink>
2018-03-24 23:06:06 -0400

    add ghost

--- blog/up.sh
+++ blog/up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . $PWD/blog/scripts/settings.sh
-docker run --rm --name $container_name -v $PWD/data/$container_name/var/lib/ghost/content:/var/lib/ghost/content -v $PWD/config/config.js:/var/lib/ghost/content/config.js -e "NODE_ENV=production" -d ghost
+docker run --rm --name $container_name -v $PWD/data/$container_name/var/lib/ghost/content:/var/lib/ghost/content -v $PWD/config/config.ghost.js:/var/lib/ghost/config.js -e "NODE_ENV=production" -d ghost
