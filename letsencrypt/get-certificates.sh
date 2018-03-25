1b6103855ff8b22fdb2f9277c5f8dea7cdc61fe3
Sam Schooler <me@sam.ink>
2018-03-24 22:29:53 -0400

    add letsencrypt

--- letsencrypt/get-certificates.sh
+++ letsencrypt/get-certificates.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-docker run --rm -p 80:80 -p 443:443 -v $PWD/etc/letsencrypt:/etc/letsencrypt -v $PWD/var/log/letsencrypt:/var/log/letsencrypt certbot/certbot certonly --standalone --agree-tos -v -c /etc/letsencrypt/cli.ini -d samschooler.me
+docker run --rm -p 80:80 -p 443:443 -v ./etc/letsencrypt:/etc/letsencrypt -v ./var/log/letsencrypt:/var/log/letsencrypt certbot/certbot certonly --standalone --agree-tos -v -c /etc/letsencrypt/cli.ini -d samschooler.me
 mkdir /etc/ssl/samschooler.me/
-cp $PWD/etc/letsencrypt/live/samschooler.me/*.pem /etc/ssl/samschooler.me/
-docker run --rm -p 80:80 -p 443:443 -v $PWD/etc/letsencrypt:/etc/letsencrypt -v $PWD/var/log/letsencrypt:/var/log/letsencrypt certbot/certbot certonly --standalone --agree-tos -v -c /etc/letsencrypt/cli.ini -d sam.ink
+cp ./etc/letsencrypt/live/samschooler.me/*.pem /etc/ssl/samschooler.me/
+docker run --rm -p 80:80 -p 443:443 -v ./etc/letsencrypt:/etc/letsencrypt -v ./var/log/letsencrypt:/var/log/letsencrypt certbot/certbot certonly --standalone --agree-tos -v -c /etc/letsencrypt/cli.ini -d sam.ink
 mkdir /etc/ssl/sam.ink/
-cp $PWD/etc/letsencrypt/live/sam.ink/*.pem /etc/ssl/sam.ink/
+cp ./etc/letsencrypt/live/sam.ink/*.pem /etc/ssl/sam.ink/
